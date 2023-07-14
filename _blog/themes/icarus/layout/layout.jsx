459cf3d01cfb1e8c2420eb51153fa1e88ab71a3d
kenzo0107 <kenzo0107@users.noreply.github.com>
2023-07-14 00:09:40 +0000

    deploy: 69d63ea6d17c758708dec03738bf6d4919caf734

--- _blog/themes/icarus/layout/layout.jsx
+++ _blog/themes/icarus/layout/layout.jsx
@@ -26,8 +26,8 @@ module.exports = class extends Component {
                                 'order-2': true,
                                 'column-main': true,
                                 'is-12': columnCount === 1,
-                                'is-8-tablet is-10-desktop is-8-widescreen': columnCount === 2,
-                                'is-8-tablet is-10-desktop is-6-widescreen': columnCount === 3
+                                'is-8-tablet is-10-desktop is-1-widescreen': columnCount === 2,
+                                'is-8-tablet is-10-desktop is-1-widescreen': columnCount === 3
                             })} dangerouslySetInnerHTML={{ __html: body }}></div>
                             <Widgets site={site} config={config} helper={helper} page={page} position={'left'} />
                         </div>
