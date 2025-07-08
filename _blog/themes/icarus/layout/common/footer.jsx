9a5b1f705d82bc41616fe69b7976d1e36e63210e
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2025-07-08 12:01:00 +0900

    広告追加

--- _blog/themes/icarus/layout/common/footer.jsx
+++ _blog/themes/icarus/layout/common/footer.jsx
@@ -55,6 +55,21 @@ class Footer extends Component {
                         </div> : null}
                     </div>
                 </div>
+                <div class="level">
+                    <div class="level-item">
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6265337967545472"
+                                crossorigin="anonymous"></script>
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-6265337967545472"
+                             data-ad-slot="2943157215"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script dangerouslySetInnerHTML={{
+                            __html: `(adsbygoogle = window.adsbygoogle || []).push({});`
+                        }}></script>
+                    </div>
+                </div>
             </div>
         </footer>;
     }
